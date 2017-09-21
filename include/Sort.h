372d68fe90dfcad54b01f9a00f92b06a33a34818
jamie401 <t106598401@ntut.org.tw>
2017-09-21 15:32:14 +0800

    9/21

--- include/Sort.h
+++ include/Sort.h
@@ -16,12 +16,15 @@ class Sort {
   // Compactness = area/perimeter
 };
 
+//template <class T> bool compare_size ( const double comX, const double comY )
+
+
 void Sort::sortByIncreasingPerimeter( std::list<Shape *> * shapeList) {
 
     for ( std::list<Shape *>::iterator iterX = shapeList->begin(); iterX != shapeList->end() ; iterX++ ) {
         for ( std::list<Shape *>::iterator iterY = shapeList->begin(); iterY != shapeList->end() ; iterY++ ) {
-            if ( iterX != iterY || (*iterX)->perimeter() < (*iterY)->perimeter() ) {
-                std::swap( iterX, iterY ) ;
+            if (  (*iterX)->perimeter() < (*iterY)->perimeter() ) {
+                std::swap( *iterX, *iterY ) ;
             }
         }
     }
@@ -29,11 +32,22 @@ void Sort::sortByIncreasingPerimeter( std::list<Shape *> * shapeList) {
 
 void Sort::sortByDecreasingPerimeter( std::list<Shape *> * shapeList) {
 
+//    shapeList->sort( compare_size ) ;
+
+//    std::sort( shapeList->begin(), shapeList->end(), (*shapeList->begin())->perimeter() ) ;
+
     for ( std::list<Shape *>::iterator iterX = shapeList->begin(); iterX != shapeList->end() ; iterX++ ) {
         for ( std::list<Shape *>::iterator iterY = shapeList->begin(); iterY != shapeList->end() ; iterY++ ) {
-            if ( iterX != iterY || (*iterX)->perimeter() > (*iterY)->perimeter() ) {
-                std::swap( iterX, iterY ) ;
+                std::cout << "----1XXX:" << (*iterX)->perimeter() << "\n" ;
+                std::cout << "----1YYY:" << (*iterY)->perimeter() << "\n" ;
+            if ( (*iterX)->perimeter() > (*iterY)->perimeter() ) {
+                std::cout << "----2XX:" << (*iterX)->perimeter() << "\n" ;
+                std::cout << "----2YY:" << (*iterY)->perimeter() << "\n" ;
+                std::swap( *iterX, *iterY ) ;
+                std::cout << "----3X:" << (*iterX)->perimeter() << "\n" ;
+                std::cout << "----3Y:" << (*iterY)->perimeter() << "\n" ;
             }
+
         }
     }
 }
@@ -42,19 +56,32 @@ void Sort::sortByIncreasingArea( std::list<Shape *> * shapeList) {
 
     for ( std::list<Shape *>::iterator iterX = shapeList->begin(); iterX != shapeList->end() ; iterX++ ) {
         for ( std::list<Shape *>::iterator iterY = shapeList->begin(); iterY != shapeList->end() ; iterY++ ) {
-            if ( iterX != iterY || (*iterX)->area() < (*iterY)->area() ) {
-                std::swap( iterX, iterY ) ;
+            if (  (*iterX)->area() < (*iterY)->area() ) {
+                std::swap( *iterX, *iterY ) ;
             }
         }
     }
+
+//    for ( std::list<Shape *>::iterator iterX = shapeList->begin(); iterX != shapeList->end() ; iterX++ ) {
+//        for ( std::list<Shape *>::iterator iterY = shapeList->begin(); iterY != shapeList->end() ; iterY++ ) {
+ //           if ( iterY == shapeList->back() || iterX == shapeList->back() ) {
+ //               if ( iterX != iterY && (*iterX)->area() < (*iterY)->area() )
+ //                   std::swap( iterX, iterY ) ;
+//                break ;
+//            }
+//            if ( iterX != iterY && (*iterX)->area() < (*iterY)->area() ) {
+//                std::swap( iterX, iterY ) ;
+//            }
+//        }
+//    }
 }
 
 void Sort::sortByDecreasingArea( std::list<Shape *> * shapeList) {
 
     for ( std::list<Shape *>::iterator iterX = shapeList->begin(); iterX != shapeList->end() ; iterX++ ) {
         for ( std::list<Shape *>::iterator iterY = shapeList->begin(); iterY != shapeList->end() ; iterY++ ) {
-            if ( iterX != iterY || (*iterX)->area() < (*iterY)->area() ) {
-                std::swap( iterX, iterY ) ;
+            if ( iterX != iterY && (*iterX)->area() < (*iterY)->area() ) {
+                std::swap( *iterX, *iterY ) ;
             }
         }
     }
@@ -64,8 +91,8 @@ void Sort::sortByIncreasingCompactness( std::list<Shape *> * shapeList) {
 
     for ( std::list<Shape *>::iterator iterX = shapeList->begin(); iterX != shapeList->end() ; iterX++ ) {
         for ( std::list<Shape *>::iterator iterY = shapeList->begin(); iterY != shapeList->end() ; iterY++ ) {
-            if ( iterX != iterY || (( (*iterX)->area() / (*iterX)->perimeter() ) < ( (*iterY)->area() / (*iterY)->perimeter()) ) ) {
-                std::swap( iterX, iterY ) ;
+            if ( iterX != iterY && (( (*iterX)->area() / (*iterX)->perimeter() ) < ( (*iterY)->area() / (*iterY)->perimeter()) ) ) {
+                std::swap( *iterX, *iterY ) ;
             }
         }
     }
--- utSort.h
+++ utSort.h
@@ -13,7 +13,8 @@ const double epsilon = 0.000001 ;
 TEST (Sort, sortByIncreasingPerimeter) {
     Rectangle r1( 0, 0, 1, 1 ) ;
     Rectangle t1( 0, 0, 2, 2 ) ;
-    Circle c1( 0, 0, 1 ) ;
+    Rectangle c1( 0, 0, 3, 3 ) ;
+//    Circle c1( 0, 0, 1 ) ;
 //    Triangle( v1, v2, v3 ) ;
 
     std::list<Shape *> shapeList ;
@@ -22,28 +23,34 @@ TEST (Sort, sortByIncreasingPerimeter) {
     shapeList.push_back (&t1) ;
     Sort sortQ1 ;
     sortQ1.sortByIncreasingPerimeter (&shapeList);
-    ASSERT_NEAR(8, shapeList.back() -> perimeter(), epsilon ); // compare the address
+//    std::cout << "-----shapeList.end:" << shapeList.back() -> perimeter() << "\n" ;
+//    std::cout << "-----shapeList.front:" << shapeList.front() -> perimeter() << "\n" ;
+    ASSERT_NEAR(12, shapeList.back() -> perimeter(), epsilon ); // compare the address
 }
 
 TEST (Sort, sortByDecreasingPerimeter) {
-    Rectangle r2(0, 0, 2, 2) ;
+    Rectangle r2( 0, 0, 2, 2) ;
     Rectangle t2( 0, 0, 3, 3 ) ;
-    Circle c2( 0, 0, 2 ) ;
+    Rectangle c2( 0, 0, 4, 4 ) ;
+//    Circle c2( 0, 0, 6 ) ;
 //    Triangle t2( ( 0, 0), ( 3, 0), ( 3, 4) ) ;
 
     std::list<Shape *> shapeList2 ;
-    shapeList2.push_back (&r2) ;
-    shapeList2.push_back (&c2) ;
+    shapeList2.push_back (&r2 ) ;
     shapeList2.push_back (&t2) ;
+    shapeList2.push_back (&c2) ;
     Sort sortQ2 ;
     sortQ2.sortByDecreasingPerimeter (&shapeList2);
-    ASSERT_NEAR(12.5663704, shapeList2.back() -> perimeter(), epsilon );
+//    std::cout << "-----shapeList.end:" << shapeList2.back() -> perimeter() << "\n" ;
+//    std::cout << "-----shapeList.front:" << shapeList2.front() -> perimeter() << "\n" ;
+    ASSERT_NEAR(16, shapeList2.back() -> perimeter(), epsilon );
 }
 
 TEST (Sort, sortByIncreasingArea) {
-    Rectangle r3(0, 0, 2, 2) ;
+    Rectangle r3( 0, 0, 2, 2 ) ;
     Rectangle t3( 0, 0, 4, 4 ) ;
-    Circle c3( 0, 0, 3 ) ;
+    Rectangle c3( 0, 0, 6, 6 ) ;
+//    Circle c3( 0, 0, 3 ) ;
 //    Triangle t3( ( 0, 0), ( 0, 5), ( 12, 5) ) ;
 
     std::list<Shape *> shapeList3 ;
@@ -52,13 +59,14 @@ TEST (Sort, sortByIncreasingArea) {
     shapeList3.push_back (&t3) ;
     Sort sortQ3 ;
     sortQ3.sortByIncreasingArea (&shapeList3);
-    ASSERT_NEAR(30, shapeList3.back() -> area(), epsilon );
+    ASSERT_NEAR(36, shapeList3.back() -> area(), epsilon );
 }
 
 TEST (Sort, sortByDecreasingArea) {
-    Rectangle r4(0, 0, 2, 2) ;
+    Rectangle r4( 0, 0, 2, 2 ) ;
     Rectangle t4( 0, 0, 5, 5 ) ;
-    Circle c4( 0, 0, 4 ) ;
+    Rectangle c4( 0, 0, 4, 4 ) ;
+//    Circle c4( 0, 0, 4 ) ;
 //    Triangle t4( ( 0, 0), ( 0, 6), ( 8, 6) ) ;
 
     std::list<Shape *> shapeList4 ;
@@ -67,13 +75,14 @@ TEST (Sort, sortByDecreasingArea) {
     shapeList4.push_back (&t4) ;
     Sort sortQ4 ;
     sortQ4.sortByDecreasingArea (&shapeList4);
-    ASSERT_NEAR(4, shapeList4.back() -> area(), epsilon );
+    ASSERT_NEAR(25, shapeList4.back() -> area(), epsilon );
 }
 
 TEST (Sort, sortByIncreasingCompactness) {
-    Rectangle r5( 0, 0, 7, 7) ;
+    Rectangle r5( 0, 0, 7, 7 ) ;
     Rectangle t5( 0, 0, 6, 6 ) ;
-    Circle c5( 0, 0, 5 ) ;
+    Rectangle c5( 0, 0, 5, 5 ) ;
+//    Circle c5( 0, 0, 5 ) ;
 //    Triangle t5( ( 0, 0), ( 7, 0), ( 7, 24) ) ;
 
     std::list<Shape *> shapeList5 ;
@@ -82,7 +91,7 @@ TEST (Sort, sortByIncreasingCompactness) {
     shapeList5.push_back (&t5) ;
     Sort sortQ5 ;
     sortQ5.sortByIncreasingCompactness (&shapeList5);
-    ASSERT_NEAR(2.5, ( shapeList5.front() -> area() / shapeList5.front() -> perimeter() ), epsilon );
+    ASSERT_NEAR(1.25, ( shapeList5.front() -> area() / shapeList5.front() -> perimeter() ), epsilon );
 }
 
 #endif
